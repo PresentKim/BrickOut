aaea4a162e46b164437809f3389a7d1ceb3b321a
PresentKim <debe3721@gmail.com>
2021-11-06 12:47:50 +0900

    Init project bone with yarn-berry

---
+++ webpack.common.js
@@ -0,0 +1,40 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const manifest = require("./site.webmanifest.json")
+
+module.exports = {
+    entry: {
+        index: "./src/index.ts"
+    },
+    resolve: {
+        extensions: [".tsx", ".ts", ".js"],
+        alias: {
+            "@": path.resolve(__dirname, "src")
+        }
+    },
+    output: {
+        publicPath: "",
+        path: path.resolve(__dirname, "dist"),
+        filename: "[name].js",
+    },
+    target: ["web", "es5"],
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: manifest.name,
+            name: manifest.name,
+            short_name: manifest.short_name,
+            favicon: "./assets/icon/favicon.ico",
+            meta: {
+                viewport: "user-scalable=no, initial-scale=1, maximum-scale=1, minimum-scale=1, width=device-width, height=device-height",
+                description: manifest.description
+            }
+        })
+    ],
+    module: {
+        rules: [
+            {test: /\.(js|tsx?|)$/i, use: ["babel-loader"]},
+            {test: /\.(css)$/i, use: ["style-loader", "css-loader"]},
+            {test: /\.(png)$/i, use: ["file-loader", "image-webpack-loader"]}
+        ]
+    }
+}
